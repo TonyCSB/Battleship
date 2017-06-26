a9d2f93a47f075b93f3251c59abd8839499ca3b2
TonyCSB <13817576706@163.com>
2017-06-26 13:23:26 +0800

    Homework

---
+++ Chapter 5/c5e14_file_words.py
@@ -0,0 +1,33 @@
+# c5e14_file_words.py
+# Tony Chen
+
+from tkinter.filedialog import askopenfilename
+
+def main():
+    print("This program counts the number of lines, words and")
+    print("characters in a file.\n")
+
+    infileName = askopenfilename()
+    
+    infile = open(infileName, "r")
+
+    lines = 0
+    words = 0
+    characters = 0
+
+    for l in infile:
+        lines = lines + 1
+
+        for w in l.split():
+            words = words + 1
+
+            for ch in w:
+                characters = characters + 1
+
+    infile.close()
+
+    print("Lines:      {0:>5}".format(lines))
+    print("Words:      {0:>5}".format(words))
+    print("Characters: {0:>5}".format(characters))
+    
+main()
