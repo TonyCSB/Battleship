94ce54117cc6dbb2c0ef351a6302b1b0b0edd0d8
TonyCSB <13817576706@163.com>
2017-07-01 13:04:01 +0800

    Sample

---
+++ Chapter 6/happy2.py
@@ -0,0 +1,15 @@
+# happy2.py
+# John Zelle
+
+def happy():
+    return "Happy Birthday to you!\n"
+
+def verseFor(person):
+    lyrics = happy() * 2 + "Happy birthday, dear " + person + ".\n" + happy()
+    return lyrics
+
+def main():
+    for person in ["Fred", "Lucy", "Elmer"]:
+        print(verseFor(person))
+
+main()
