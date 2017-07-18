82015ee7364e712217542e696c7c62878de72820
TonyCSB <13817576706@163.com>
2017-07-18 23:30:18 +0800

    Homework

--- Chapter 8/c8e2_windchill.py
+++ Chapter 8/c8e2_windchill.py
@@ -5,17 +5,27 @@ def calc(t, v):
     a = 35.75 + 0.6215 * t - 35.75 * v ** 0.16 + 0.4275 * t * v ** 0.16
     return a
 
+def printHead():
+    print("╔══", end = "")
+    print("╦══" * 11, end = "")
+    print("╗")
+
+    print("║    ", end = "")
+    for i in range (0, 55, 5):
+        print("║{0:^4.0f}".format(i), end = "")
+    print("║")
+
+    print("╠══", end = "")
+    print("╬══" * 11, end = "")
+    print("╣")
+
 def main():
     print("This program generates a table of windchill values, with rows representing")
     print("wind speed (miles per hour) and columns representing temperature (degrees")
     print("Fahrenheit).\n")
-    
-    print("╔══╦══╦══╦══╦══╦══╦══╦══╦══╦" +
-          "══╦══╦══╗\n" +
-          "║    ║  0 ║  5 ║ 10 ║ 15 ║ 20 ║ 25 ║ 30 ║ 35 ║" +
-          " 40 ║ 45 ║ 50 ║\n" +
-          "╠══╬══╬══╬══╬══╬══╬══╬══╬══╬" +
-          "══╬══╬══╣")
+
+    printHead()
+
     t = -20
 
     while t != 70:
